d456eafdd126e26bb24ded703966eaf47ab3c77f
mohamoud ali <103631772+mahashali@users.noreply.github.com>
2023-08-02 07:34:47 -0600

    Add files via upload

---
+++ upload.php
@@ -0,0 +1,21 @@
+<?php
+	require_once 'conn.php';
+	if(ISSET($_POST['upload'])){
+		$image_name = $_FILES['image']['name'];
+		$image_temp = $_FILES['image']['tmp_name'];
+		$exp = explode(".", $image_name);
+		$end = end($exp);
+		$name = time().".".$end;
+		$path = "upload/".$name;
+		$allowed_ext = array("gif", "jpg", "jpeg", "png");
+		if(in_array($end, $allowed_ext)){
+			if(move_uploaded_file($image_temp, $path)){
+				mysqli_query($conn, "INSERT INTO `image` VALUE('', '$name', '$pa')") or die(mysqli_error());	
+				echo "<script>alert('Image saved!')</script>";
+				header("location: index.php");
+			}
+		}else{
+			echo "<script>alert('Image only')</script>";
+		}
+	}
+?>
